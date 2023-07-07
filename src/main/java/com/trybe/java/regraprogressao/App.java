668264fd723c89ac6087c4828c04390cd768185f
TeksMarques <taotaodoce@gmail.com>
2023-07-07 15:10:09 -0300

    feat: req 1 concluido

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,13 +1,39 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * App.
  */
 public class App {
-
   /**
    * Metodo main.
    */
   public static void main(String[] args) {
+
+    Scanner leitor = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int atividades = Integer.parseInt(leitor.nextLine());
+
+    int question;
+    ArrayList<String> listaAtividades = new ArrayList<String>();
+    ArrayList<Integer> listaPesos = new ArrayList<Integer>();
+
+    for (question = 1; question <= atividades; question += 1) {
+      System.out.println("Digite o nome da atividade " + question);
+      String nomeAtividade = leitor.nextLine();
+      listaAtividades.add(nomeAtividade);
+      System.out.println("Digite o peso da atividade " + question);
+      int peso = Integer.parseInt(leitor.nextLine());
+      listaPesos.add(peso);
+    }
+
+    int somaPesos = 0;
+
+    for (int peso : listaPesos) {
+      somaPesos += peso;
+    }
   }
 }
